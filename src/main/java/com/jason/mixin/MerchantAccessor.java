8dda779cfe2b5b5745c35c43cbe3d4ffff274b41
RadioactiveGorilla69 <mindcraf.player@gmail.com>
2025-04-27 11:17:32 -0700

    adding trades to level up feature
    im trying lol

---
+++ src/main/java/com/jason/mixin/MerchantAccessor.java
@@ -0,0 +1,12 @@
+package com.jason.mixin;
+
+import net.minecraft.screen.MerchantScreenHandler;
+import net.minecraft.village.Merchant;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(MerchantScreenHandler.class)
+public interface MerchantAccessor {
+    @Accessor("merchant")
+    Merchant getMerchant();
+}
--- src/main/java/com/jason/mixin/TradeXpMixin.java
+++ src/main/java/com/jason/mixin/TradeXpMixin.java
@@ -16,34 +16,49 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MerchantScreen.class)
 public abstract class TradeXpMixin extends Screen {
-	protected TradeXpMixin(Text title) {
-		super(title);
-	}
+    protected TradeXpMixin(Text title) {
+        super(title);
+    }
 
-	@Inject(method = "render", at = @At("RETURN"))
-	private void renderXp(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
-		MerchantScreen merchantScreen = (MerchantScreen)(Object)this;
-		MerchantScreenHandler handler = merchantScreen.getScreenHandler();
-		TradeOfferList offers = handler.getRecipes();
-		TextRenderer font = MinecraftClient.getInstance().textRenderer;
-		int startIndex = ((MerchantScreenAccessor)merchantScreen).getIndexStartOffset();
+    @Inject(method = "render", at = @At("RETURN"))
+    private void renderXpAndTradeLevel(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        MerchantScreen merchantScreen = (MerchantScreen) (Object) this;
+        MerchantScreenHandler handler = merchantScreen.getScreenHandler();
+        TradeOfferList offers = handler.getRecipes();
+        TextRenderer font = MinecraftClient.getInstance().textRenderer;
+        int startIndex = ((MerchantScreenAccessor) merchantScreen).getIndexStartOffset();
 
-		for(int i = 0; i < Math.min(offers.size() - startIndex, 7); i++) {
-			TradeOffer offer = offers.get(i + startIndex);
+        int x = this.width / 2 - 158;
 
-			int x = this.width / 2 - 158;
-			int y = this.height / 2 - 58 + (i * 20);
+        int merchantXp = handler.getExperience();
+        int requiredXp = 0;
 
-			context.drawText(
-					font,
-					String.valueOf(offer.getMerchantExperience()),
-					x,
-					y,
-					0xFFFFFF,
-					false
-			);
+        if (merchantXp >= 0) {
+            requiredXp = 10 - merchantXp;
+        } else if (merchantXp >= 10) {
+            requiredXp = 60 - merchantXp;
+        } else if (merchantXp >= 70) {
+            requiredXp = 80 - merchantXp;
+        } else if (merchantXp >= 150) {
+            requiredXp = 100 - merchantXp;
+        } else if (merchantXp >= 250) {
+            requiredXp = 0;
+        }
 
-			context.drawText(font, "XP", x, this.height/2 - 77, 0xFFFFFF, false);
-		}
-	}
+        System.out.println("required: " + requiredXp);
+        System.out.println("merchant: " + merchantXp);
+
+        for (int i = 0; i < Math.min(offers.size() - startIndex, 7); i++) {
+            TradeOffer offer = offers.get(i + startIndex);
+            int tradesToLevelUp = (int) Math.ceil((double) (requiredXp - merchantXp) / offer.getMerchantExperience());
+            int y = this.height / 2 - 58 + (i * 20);
+
+            context.drawText(font, String.valueOf(offer.getMerchantExperience()), x, y, 0xFFFFFF, false);
+            context.drawText(font, String.valueOf(tradesToLevelUp), x - 100, y, 0xFFFFFF, false);
+        }
+        if (requiredXp != 0) {
+            context.drawText(font, "Villager trades to level up", x - 100, this.height / 2 - 77, 0xFFFFFF, false);
+        }
+        context.drawText(font, "XP", x, this.height / 2 - 77, 0xFFFFFF, false);
+    }
 }
