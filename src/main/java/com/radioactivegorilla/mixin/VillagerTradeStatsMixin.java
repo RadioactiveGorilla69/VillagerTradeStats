4b80cf1943843a9d681f66b24bc30a03ecded175
RadioactiveGorilla69 <mindcraf.player@gmail.com>
2025-04-28 22:26:30 -0700

    GUI changes
    made it more natural by combing demo background and villager background. Also extended it to the very bottom. And also organized some code. Also changed from "Trades to next level" to "Level Up" and added tooltip

--- src/main/java/com/radioactivegorilla/mixin/VillagerTradeStatsMixin.java
+++ src/main/java/com/radioactivegorilla/mixin/VillagerTradeStatsMixin.java
@@ -1,5 +1,6 @@
 package com.radioactivegorilla.mixin;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
@@ -7,6 +8,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.MerchantScreen;
 import net.minecraft.screen.MerchantScreenHandler;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.TradeOfferList;
 import org.spongepowered.asm.mixin.Mixin;
@@ -17,8 +19,29 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MerchantScreen.class)
 public abstract class VillagerTradeStatsMixin extends Screen {
+
     protected VillagerTradeStatsMixin(Text title) {
         super(title);
+    }
+
+    @Inject(method = "render", at = @At("RETURN"))
+    private void renderBackground(DrawContext context, int mouseX, int mouseY, float delta, CallbackInfo ci) {
+        MerchantScreen merchantScreen = (MerchantScreen) (Object) this;
+        MerchantScreenHandler handler = merchantScreen.getScreenHandler();
+        TradeOfferList offers = handler.getRecipes();
+
+        if (!isWanderingTrader(offers)) {
+            int x = this.width/2 - 153;
+            int backgroundHeight = Math.min(offers.size(), 7) * 20 + 26;
+            //int displacementFit = offers.size() >= 7 ? 7 : 0;
+
+            int merchantXp = handler.getExperience();
+            if (merchantXp >= 250) {
+                drawThinBackground(context, x - 8, this.height/2 - 83, 26, backgroundHeight);
+            } else {
+                drawThickBackground(context, x - 62, this.height/2 - 83, 80, 7 * 20 + 26);
+            }
+        }
     }
 
     @Inject(method = "render", at = @At("RETURN"))
@@ -29,58 +52,51 @@ public abstract class VillagerTradeStatsMixin extends Screen {
         TextRenderer font = MinecraftClient.getInstance().textRenderer;
         int startIndex = ((MerchantScreenAccessor) merchantScreen).getIndexStartOffset();
 
-        int x = this.width / 2 - 153;
-
+        int x = this.width/2 - 153;
         int merchantXp = handler.getExperience();
 
         if (!isWanderingTrader(offers)) {
             int requiredXp;
 
-            if (merchantXp > 250) {
+            if (merchantXp >= 250) {
                 requiredXp = 0;
-            } else if (merchantXp >= 150) {
+            }
+            else if (merchantXp >= 150) {
                 requiredXp = 250 - merchantXp;
-            } else if (merchantXp >= 70) {
+            }
+            else if (merchantXp >= 70) {
                 requiredXp = 150 - merchantXp;
-            } else if (merchantXp >= 10) {
+            }
+            else if (merchantXp >= 10) {
                 requiredXp = 70 - merchantXp;
-            } else {
+            }
+            else {
                 requiredXp = 10 - merchantXp;
             }
 
-            int tradesToLevelUp;
             int highestXPperTrade = getHighestXpPerTrade(offers, startIndex);
+
+            if (requiredXp != 0) {
+                context.drawText(font, "Level Up", x - 54, this.height/2 - 77, 0xFF404040, false);
+                NumTradesToNextLevelTooltip(context, "Level Up", x - 54, this.height/2 - 77, font, mouseX, mouseY);
+            }
 
             for (int i = 0; i < Math.min(offers.size() - startIndex, 7); i++) {
                 TradeOffer offer = offers.get(i + startIndex);
-                tradesToLevelUp = (int) Math.ceil((double) (requiredXp) / offer.getMerchantExperience());
-                int y = this.height / 2 - 58 + (i * 20);
+                int tradesToLevelUp = (int) Math.ceil((double)(requiredXp)/offer.getMerchantExperience());
+                int y = this.height/2 - 58 + (i * 20);
 
-                int color = (offer.getMerchantExperience() == highestXPperTrade) ? 0x09eb10 : 0xFFFFFF;
+                int color = (offer.getMerchantExperience() == highestXPperTrade) ? 0x09eb10 : 0xFF404040;
                 boolean bold = (offer.getMerchantExperience() == highestXPperTrade);
 
                 context.drawText(font, String.valueOf(offer.getMerchantExperience()), x, y, color, bold);
                 if (requiredXp != 0) {
-                    context.drawText(font, String.valueOf(tradesToLevelUp), x - 120 + (font.getWidth("Trades to next level") - font.getWidth(String.valueOf(tradesToLevelUp))) / 2, y, color, bold);
+                    context.drawText(font, String.valueOf(tradesToLevelUp), x - 54 + (font.getWidth("Level Up"))/2, y, color, bold);
                 }
             }
-            /*if(offers.size() > 7 && requiredXp != 0){
-                context.drawText(font, "Trades to next level", x - 120, this.height / 2 - 77, 0xFFFFFF, false);
-                context.drawBorder(x - 125, this.height / 2 - 83, 135, Math.min(offers.size(), 7) * 20 + 25, 0xFF000000);
-            }*/
-            if(requiredXp == 0){
-                context.drawBorder(x - 5, this.height / 2 - 83, 20, Math.min(offers.size(), 7) * 20 + 25, 0xFF000000);
-            }
-            else{
-                int displacementFit = 0;
-                if(offers.size() >= 7){
-                    displacementFit = 7;
-                }
-                context.drawText(font, "Trades to next level", x - 120, this.height / 2 - 77, 0xFFFFFF, false);
-                context.drawBorder(x - 125, this.height / 2 - 83, 140, Math.min(offers.size(), 7) * 20 + 18 + displacementFit, 0xFF000000);
-            }
-            context.drawText(font, "XP", x, this.height / 2 - 77, 0xFFFFFF, true);
-            context.drawText(font, "Villager XP: " + handler.getExperience(), (this.width - font.getWidth("Villager XP: " + handler.getExperience())) / 2, this.height / 4 + 35, 0xFFFFFF, true);
+
+            context.drawText(font, "XP", x, this.height/2 - 77, 0xFF404040, false);
+            context.drawText(font, "Villager XP: " + handler.getExperience(), (this.width - font.getWidth("Villager XP: " + handler.getExperience()))/2, this.height/4 + 35, 0xFFFFFF, true);
         }
     }
 
@@ -107,4 +123,27 @@ public abstract class VillagerTradeStatsMixin extends Screen {
         return true;
     }
 
+    @Unique
+    private void drawThinBackground(DrawContext context, int x, int y, int width, int height) {
+        Identifier thin_background = Identifier.of("villagertradestats", "textures/gui/thin_background.png");
+        RenderSystem.setShaderTexture(0, thin_background);
+        context.drawTexture(thin_background, x, y, 0, 0, width, height, width, 256);
+    }
+
+    @Unique
+    private void drawThickBackground(DrawContext context, int x, int y, int width, int height) {
+        Identifier thick_background = Identifier.of("villagertradestats", "textures/gui/thick_background.png");
+        RenderSystem.setShaderTexture(0, thick_background);
+        context.drawTexture(thick_background, x, y, 0, 0, width, height, width, 256);
+    }
+
+    @Unique
+    private void NumTradesToNextLevelTooltip(DrawContext context, String text, int x, int y, TextRenderer font, int mouseX, int mouseY) {
+        int textWidth = font.getWidth(text);
+        int textHeight = 10;
+
+        if(mouseX >= x && mouseX <= x + textWidth && mouseY >= y && mouseY <= y + textHeight) {
+            context.drawTooltip(MinecraftClient.getInstance().textRenderer, Text.of("Number of trades until the villager levels up"), mouseX, mouseY);
+        }
+    }
 }
